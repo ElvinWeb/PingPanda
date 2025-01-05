bb7e94edeae3aa4776c230b9458143716aa87287
Elvin <elvinsrkrov@gmail.com>
2025-01-05 11:05:21 +0400

    Account settings page has added

--- src/server/routers/project-router.ts
+++ src/server/routers/project-router.ts
@@ -3,6 +3,7 @@ import { db } from "@/db"
 import { addMonths, startOfMonth } from "date-fns"
 import { router } from "../__internals/router"
 import { privateProcedure } from "../procedures"
+import { z } from "zod"
 
 export const projectRouter = router({
   getUsage: privateProcedure.query(async ({ c, ctx }) => {
@@ -37,4 +38,17 @@ export const projectRouter = router({
     })
   }),
 
+  setDiscordID: privateProcedure
+    .input(z.object({ discordId: z.string().max(20) }))
+    .mutation(async ({ c, ctx, input }) => {
+      const { user } = ctx
+      const { discordId } = input
+
+      await db.user.update({
+        where: { id: user.id },
+        data: { discordId },
+      })
+
+      return c.json({ success: true })
+    }),
 })
